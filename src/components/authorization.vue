bdf9d3eaa4a9b2e0875f823ba5400f8d22183188
Tulen41k <tu1en41k@yandex.ru>
2022-12-21 18:02:45 +0300

    Создан макет для форм авторизации и регистрации

--- src/components/authorization.vue
+++ src/components/authorization.vue
@@ -1,28 +1,61 @@
 <template>
     <div class="authorization">
-        <p>Ещё не зарегистрировались? Нажмите</p>
-        <button>registration</button>
-        
-        <registration/>
+        <p>Вход в личный аккаунт</p>
+        <p>
+            <input id="login" v-model="login" placeholder="Логин"/>
+        </p>
+        <p>
+            <input id="password" v-model="password" placeholder="Пароль"/>
+        </p>
+        <button>Войти</button>
+        <p class="href">Нет аккаунта? Создать</p>
     </div>
 </template>
 
 <script>
-    import registration from './registration'
 
 export default {
     name: 'authorization',
     components: {
-        registration
     },
     props: {},
     data() {
-        return {}
+        return {
+            login: null,
+            password: null
+        }
     },
     conputed: {}
 }
 </script>
 
-<style scoped>
+<style>
+    .authorization {
+        display: flex;
+        flex-direction: column;
+        height: 80%;
+        width: 60%;
+        min-height: 400px;
+        background-color: rgb(248, 248, 248);
+    }
+
+    input {
+        background-color: rgb(226, 226, 226);
+        width: 85%;
+        padding: 5px;
+    }
+
+    button {
+        width: 85%;
+        padding: 5px;
+        align-self: center;
+        margin-top: 10px;
+    }
+
+    .href {
+        align-self: flex-end;
+        margin-right: 15px;
+        color: blue;
+    }
 
 </style>
--- src/components/registration.vue
+++ src/components/registration.vue
@@ -1,28 +1,76 @@
 <template>
     <div class="registration">
-        <p>Если уже зарегистрированы, нажмите</p>
-        <button>Authorization</button>
-
-        <authorization/>
+        <p>Регистрация нового пользователя</p>
+        <p>
+            <input class="name" id="name" v-model="name" placeholder="Имя"/>
+            <input class="surname" id="surname" v-model="surname" placeholder="Фамилия"/>
+        </p>
+        <p>
+            <input class="login" id="login" v-model="login" placeholder="Логин"/>
+        </p>
+        <p>
+            <input class="password" id="password" v-model="password" placeholder="Пароль"/>
+        </p>
+        <button>Зарегистрироваться</button>
+        <p class="href">Уже есть аккаунт? Войти</p>
     </div>
 </template>
 
 <script>
-    import authorization from './authorization';
 
 export default {
     name: 'registration',
     components: {
-        authorization
     },
     props: {},
     data() {
-        return {}
+        return {
+            name: null,
+            surname: null,
+            login: null,
+            password: null
+        }
     },
     conputed: {}
 }
 </script>
 
 <style scoped>
+    .registration {
+        display: flex;
+        flex-direction: column;
+        height: 80%;
+        width: 60%;
+        min-height: 400px;
+        background-color: rgb(248, 248, 248);
+    }
+
+    .name,
+    .surname {
+        background-color: rgb(226, 226, 226);
+        padding: 5px;
+        width: 40%;
+        margin: 1.5%;
+    }
+
+    .login,
+    .password {
+        background-color: rgb(226, 226, 226);
+        width: 85%;
+        padding: 5px;
+    }
+
+    button {
+        width: 85%;
+        padding: 5px;
+        align-self: center;
+        margin-top: 10px;
+    }
+
+    .href {
+        align-self: flex-end;
+        margin-right: 15px;
+        color: blue;
+    }
 
 </style>
