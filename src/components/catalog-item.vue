acf55aa04089aee14e8784ff626e124e3c015e33
Козлова Виктория Ильинична <v.kozlova@nicetu.spb.ru>
2022-12-28 12:29:39 +0300

    Разработан макет карточки товара

--- src/components/catalog-item.vue
+++ src/components/catalog-item.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="catalogItem">
-        <p>Item 1</p>
-        <p> Price 100</p>
-        <p>Add to cart</p>
+        <img class="image" src="https://klike.net/uploads/posts/2020-07/1594278030_1.jpg"/>
+        <div class="box-description">
+            <p class="title">Название телефона</p>
+            <p class="description">Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. 
+                Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. 
+                Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. Большое описание. 
+            </p>
+        </div>
+        <p class="isHave">В наличии</p>
+        <div class="box-price">
+            <p class="price">20 533 р.</p>
+            <button class="btn-cart">В корзину</button>
+        </div>
     </div>
 </template>
 
@@ -20,7 +30,48 @@
 <style scoped>
     .catalogItem {
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
+        align-items: center;
+
+    }
+
+    .image {
+        height: 15%;
+        width: 13%;
+    }
+
+    .box-description {
+        width: 65%;
+    }
+
+    .box-price {
+        width: 15%;
+    }
+
+    .price {
+        font-size: smaller;
+    }
+
+    .isHave {
+        width: 7%;
+        font-size: small;
+        color: rgb(64, 202, 64);
+    }
+
+    .title {
+        text-align: left;
+        color: rgb(64, 202, 64);
+    }
+
+    .description {
+        text-align: left;
+    }
+
+    .btn-cart {
+        background-color: rgba(45, 101, 255, 0.603);
+        color: white;
+        padding: 2%;
+        border: none;
     }
 
 
